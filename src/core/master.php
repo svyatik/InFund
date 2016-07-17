7119e9e5eb88a9c5787cc056a0d5da2362174531
Maxym Naumchyk <max.naumch@gmail.com>
2016-07-17 12:55:19 +0300

    New Interpreter class

--- src/core/master.php
+++ src/core/master.php
@@ -1,1 +1,12 @@
 <?php
+
+class Interpreter {
+
+    /// Interpret some command and execute it. This will possibly change
+    /// the database or other internal data.
+    ///
+    /// $cmd - a command list to interpret.
+    public function interpret($cmd) {
+        // TODO: unimplemented!
+    }
+}

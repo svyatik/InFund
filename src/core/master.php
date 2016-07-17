303bb7d711b1f37d5e13ef680f8737371d38b058
Maxym Naumchyk <max.naumch@gmail.com>
2016-07-17 13:02:10 +0300

    New Token class

--- src/core/master.php
+++ src/core/master.php
@@ -1,5 +1,26 @@
 <?php
 
+/// Class represents a single token from the received command string.
+class Token {
+    var $data;
+    var $type;
+
+    public function Token($data, $type) {
+        $this->data = $data;
+        $this->type = $type;
+    }
+
+    /// Data is a string with actual read token.
+    public function data() {
+        return $this->data;
+    }
+
+    /// Type is a token type from an enumeration of types.
+    public function type() {
+        return $this->type;
+    }
+}
+
 class Interpreter {
 
     /// Interpret some command and execute it. This will possibly change

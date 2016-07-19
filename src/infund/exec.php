775dcf2cca07a89f33f3eacbc0cca9e4d8ecbaa2
Maxym Naumchyk <max.naumch@gmail.com>
2016-07-19 21:21:00 +0300

    Added basic command list class Cmds

--- src/infund/exec.php
+++ src/infund/exec.php
@@ -1,1 +1,14 @@
 <?php header('Content-type: charset=utf-8');
+/*
+
+This file contains the executor of the command code.
+
+*/
+
+/// This class contains all command codes.
+/// Cmds stands for Commands.
+class Cmds {
+
+    const C_CREATE_FUND             = 0x0001;
+
+}
